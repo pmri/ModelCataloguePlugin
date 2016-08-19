8038d88bd677ba90142a0c68ae8488ff10da41be
Adam Milward <adam.milward@outlook.com>
2016-08-19 15:27:14 +0100

    rare disease import should pick up gel id

--- ModelCatalogueGenomicsPlugin/grails-app/services/org/modelcatalogue/gel/RareDiseaseImportService.groovy
+++ ModelCatalogueGenomicsPlugin/grails-app/services/org/modelcatalogue/gel/RareDiseaseImportService.groovy
@@ -33,9 +33,9 @@ class RareDiseaseImportService {
                     Preconditions.checkElementIndex(10, tokens.length, "Unexpected number of column in csv export, minimum number is 10")
                     // check existence of data classes and their belonging to data model
                     [
-                        [level: 2, id: tokens[0], dataClass: elementService.findByModelCatalogueId(DataClass, expandToLocalIdWithoutVersion(tokens[0]))],
-                        [level: 3, id: tokens[2], dataClass: elementService.findByModelCatalogueId(DataClass, expandToLocalIdWithoutVersion(tokens[2]))],
-                        [level: 4, id: tokens[4], dataClass: elementService.findByModelCatalogueId(DataClass, expandToLocalIdWithoutVersion(tokens[4]))],
+                        [level: 2, id: tokens[0], dataClass: getGelIDClass(dataModelToImport, tokens[0].toString())],
+                        [level: 3, id: tokens[2], dataClass: getGelIDClass(dataModelToImport, tokens[2].toString())],
+                        [level: 4, id: tokens[4], dataClass: getGelIDClass(dataModelToImport, tokens[4].toString())],
                     ].each {
                         Preconditions.checkNotNull(it.dataClass, "Cannot find level ${it.level} data class with id ${it.id}")
                         Preconditions.checkArgument(it.dataClass.dataModel != dataModelToImport, "Level ${it.level} " +
@@ -56,7 +56,7 @@ class RareDiseaseImportService {
                                 // add clinical test relationship if specified in csv
                                 if (tokens[10]) {
                                     // create clinical test if not present
-                                    def clinicalTest = getClinicalTest(testDataModel, tokens[10])
+                                    def clinicalTest = getGelIDClass(testDataModel, tokens[10])
                                     // throw exception when there is not clinical test data class
                                     Preconditions.checkNotNull(clinicalTest, "Cannot find data class with given id ${tokens[10]} for Clinical Tests data model ${testDataModel}")
                                     rel 'hierarchy' to clinicalTest.dataModel.name, clinicalTest.name
@@ -82,15 +82,17 @@ class RareDiseaseImportService {
         ElementService.getLatestFromCriteria(criteria) as DataClass
     }
 
-    private DataClass getClinicalTest(DataModel dataModel, String clinicalTestId) {
-        def localId = expandToLocalIdWithoutVersion(clinicalTestId)
-        def clinicalTest = elementService.findByModelCatalogueId(DataClass, localId) ?: CatalogueElementProxyRepository.findByMissingReferenceId(localId)
-
-        if (clinicalTest && clinicalTest.dataModel.id == dataModel.id) {
-            return clinicalTest
+    static DataClass getGelIDClass(DataModel dataModel, String gelID) {
+        DetachedCriteria<DataClass> criteria = new DetachedCriteria<DataClass>(DataClass).build {
+            extensions {
+                eq 'name', 'http://www.modelcatalogue.org/metadata/genomics/#gel-id'
+                eq 'extensionValue', gelID
+            }
+            eq 'dataModel', dataModel
         }
-        return null
+        ElementService.getLatestFromCriteria(criteria) as DataClass
     }
+
 
     private String expandToLocalIdWithoutVersion(String shortId) {
         // get rid of version - version might be after dot or at character
